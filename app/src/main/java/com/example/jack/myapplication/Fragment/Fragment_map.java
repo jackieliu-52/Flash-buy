70c308c467d0a47e0b3102ce0bff0ab71b226249
Jackliu <878923730@qq.com>
2016-08-16 19:52:08 +0800

    备份

--- app/src/main/java/com/example/jack/myapplication/Fragment/Fragment_map.java
+++ app/src/main/java/com/example/jack/myapplication/Fragment/Fragment_map.java
@@ -9,6 +9,10 @@ import android.view.ViewGroup;
 import com.example.jack.myapplication.R;
 
 /**
+ * 因为虽然这边用viewpager去管理，而且指示器用的是弱引用去管理，但是还是不需要
+ * 用单例模式去管理（其实也不知道怎么管理，除非换一个其他指示器库）
+ * 因为将它放在三个pager的中间，一开始在看第一个pager的时候就已经开始预加载了，
+ * 同时因为Fragment_buy是单例模式，所以这个fragment相当于一个单例模式（很奇怪）
  * Created by Jack on 2016/8/5.
  */
 public class Fragment_map extends android.support.v4.app.Fragment {
@@ -20,5 +24,25 @@ public class Fragment_map extends android.support.v4.app.Fragment {
     {
         Log.i(TAG,"onCreateView");
         return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.i(TAG,"onResume");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.i(TAG,"onPause");
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser) {
+            //相当于Fragment的onResume
+        } else {
+            //相当于Fragment的onPause
+        }
     }
 }
